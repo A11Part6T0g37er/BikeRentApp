3b12b7457195881379e6aa00ba6e9c87ac1a069c
tt10 <tt10is7ios@gmail.com>
2021-02-23 21:01:20 +0200

    Add project files.

---
+++ Models/DAL/Repository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeRentApp.Models.DAL
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+        protected readonly DbSet<TEntity> Entities;
+
+        public Repository(DbContext context)
+        {
+            Context = context;
+            Entities = Context.Set<TEntity>();
+        }
+
+        public void Add(TEntity entity)
+        {
+            Entities.Add(entity);
+        }
+    }
+}
